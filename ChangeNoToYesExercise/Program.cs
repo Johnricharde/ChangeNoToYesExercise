a5090a3411dfaa6b47b2d8b06558847e881289b8
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-16 19:30:41 +0100

    Add project files.

---
+++ ChangeNoToYesExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ChangeNoToYesExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Why take no for an answer?
+            string userInput = Console.ReadLine();
+            string agreeableUserInput = userInput.Replace("no", "yes", StringComparison.OrdinalIgnoreCase);
+            Console.WriteLine(agreeableUserInput);
+        }
+    }
+}
